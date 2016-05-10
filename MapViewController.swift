012b062a082affd9dccb28e72fa81f05a5b8bc2e
student3 <student3@jh-mbair-158-12.d214.org>
2016-05-10 14:05:37 -0500

    Re-added Geo coder

--- MapViewController.swift
+++ MapViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 import MapKit
-
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var theMapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    var locationAdress: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        theMapView.delegate = self
+        
+        let location = locationAdress
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(location) { (arrayOfPlacemarks, Error) -> Void in
+            for place in arrayOfPlacemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.theMapView.addAnnotation(annotation)
+                
+            }
+        }
+        theMapView.showsUserLocation = true
     }
     
+    
+}
 
-    }
